cbd0f94b5a0cd9b165bc2df375aa2c79b6cf6dd9
atuna26 <91380434+atuna26@users.noreply.github.com>
2025-12-05 11:46:51 +0100

    camelcase

--- src/homeLandingPage/components/Main.jsx
+++ src/homeLandingPage/components/Main.jsx
@@ -1,6 +1,5 @@
 
 import { useState } from 'react'
-import AboutUs from './AboutUs.JSX'
 import BlockGallery from './BlockGallery'
 import Factory from './Factory'
 import Fair from './Fair'
@@ -8,11 +7,8 @@ import Hero from './Hero'
 import Mine from './Mine'
 import News from './News'
 import PaverGallery from './PaverGallery'
-import Product from './Product'
-import References from './References'
 import SlabGallery from './SlabGallery'
 import TileGallery from './TileGallery'
-import TripleInfo from './TripleInfo'
 const Main = () => {
   return (
     <div className='grid grid-cols-2 gap-5 w-full'> 
